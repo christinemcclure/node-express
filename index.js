a8c21549f8e652678982b3cc3eb6758688d43415
Christine McClure <camcclure@gmail.com>
2023-01-11 08:46:37 -0600

    stopped at handlebars section

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const exphbs = require('express-handlebars');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const PORT = process
+
+// Set handlebars middleware 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Set Handlebar routes
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
+app.listen(PORT, () => console.log('Server listening on port' + PORT));
 

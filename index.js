29c5b7f95d7a56b567ffc91aefdc5bd3b7a39d3f
Christine McClure <camcclure@gmail.com>
2023-01-13 11:00:18 -0600

    through 2-14

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
-const PORT = process
+const PORT = process.env.port || 5000
+
+const otherstuff="this is other stuff..."
 
 // Set handlebars middleware 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 // Set Handlebar routes
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home', {
+        stuff: otherstuff
+    });
 });
 
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(PORT, () => console.log('Server listening on port' + PORT));
+app.listen(PORT, () => console.log('Server listening on port ' + PORT));
 
